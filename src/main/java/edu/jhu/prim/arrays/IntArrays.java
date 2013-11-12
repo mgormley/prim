d6582c73add014064061f3de402cd7a912201189
Matt Gormley <mrg@cs.jhu.edu>
2013-11-12 10:15:20 -0500

    Adding asArray and count methods.

--- src/main/java/edu/jhu/prim/arrays/IntArrays.java
+++ src/main/java/edu/jhu/prim/arrays/IntArrays.java
@@ -128,4 +128,15 @@ public class IntArrays {
         }
     }
 
+    /** Counts the number of times value appears in array. */
+    public static int count(int[] array, int value) {
+        int count = 0;
+        for (int i=0; i<array.length; i++) {
+            if (array[i] == value) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
 }
