9abc1afeb651765fa918472a99b5bfd6e1bd20f1
Matt Gormley <mrg@cs.jhu.edu>
2014-01-27 14:05:51 -0500

    Adding cast methods for shorts

--- src/main/java/edu/jhu/prim/util/SafeCast.java
+++ src/main/java/edu/jhu/prim/util/SafeCast.java
@@ -5,7 +5,14 @@ public class SafeCast {
     private SafeCast() {
         // Private constructor.
     }
-
+    
+    public static short safeIntToShort(int i) {
+        if (i > (int)Short.MAX_VALUE) {
+            throw new IllegalStateException("Cannot convert int to short: " + i);
+        }
+        return (short)i;
+    }
+    
     public static int safeLongToInt(long l) {
         if (l > (long)Integer.MAX_VALUE) {
             throw new IllegalStateException("Cannot convert long to int: " + l);
@@ -18,6 +25,13 @@ public class SafeCast {
             throw new IllegalStateException("Cannot convert double to int: " + d);
         }
         return (int)d;
+    }
+
+    public static float safeDoubleToFloat(double d) {
+        if (d > (double)Float.MAX_VALUE) {
+            throw new IllegalStateException("Cannot convert double to float: " + d);
+        }
+        return (float)d;
     }
     
     public static int[] safeLongToInt(long[] longArray) {
