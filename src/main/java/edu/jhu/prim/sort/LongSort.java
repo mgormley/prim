946711b2a0a0b8ab99e9edba209cab8c1648d9ff
Matt Gormley <mrg@cs.jhu.edu>
2014-04-01 23:25:25 -0400

    Switching to non-recursive quicksort.

--- src/main/java/edu/jhu/prim/sort/LongSort.java
+++ src/main/java/edu/jhu/prim/sort/LongSort.java
@@ -1,6 +1,7 @@
 package edu.jhu.prim.sort;
 
 import edu.jhu.prim.arrays.LongArrays;
+import edu.jhu.prim.list.IntStack;
 
 public class LongSort {
 
@@ -14,9 +15,9 @@ public class LongSort {
      * Performs an in-place quick sort on array. Sorts in descending order.
      */
     public static void sortDesc(long[] array) {
-        LongArrays.scale(array, -1);
+        LongArrays.scale(array, (long) -1);
         sortAsc(array);
-        LongArrays.scale(array, -1);
+        LongArrays.scale(array, (long) -1);
     }
     
     /**
@@ -27,6 +28,33 @@ public class LongSort {
     }
     
     private static void quicksort(long[] array, int left, int right) {
+        IntStack leftStack = new IntStack();
+        IntStack rightStack = new IntStack();
+        leftStack.add(left);
+        rightStack.add(right);
+        while (leftStack.size() > 0) {
+            left = leftStack.pop();
+            right = rightStack.pop();
+            if (left < right) {
+                // Choose a pivot index.
+                // --> Here we choose the rightmost element which does the least
+                // amount of work if the array is already sorted.
+                int pivotIndex = right;
+                // Partition the array so that everything less than
+                // values[pivotIndex] is on the left of pivotNewIndex and everything
+                // greater than or equal is on the right.
+                int pivotNewIndex = partition(array, left, right, pivotIndex);
+                // "Recurse" on the left side.
+                leftStack.push(left);
+                rightStack.push(pivotNewIndex - 1);
+                // "Recurse" on the right side.
+                leftStack.push(pivotNewIndex + 1);
+                rightStack.push(right);
+            }
+        }
+    }
+    
+    static void quicksortRecursive(long[] array, int left, int right) {
         if (left < right) {
             // Choose a pivot index.
             // --> Here we choose the rightmost element which does the least
@@ -60,10 +88,23 @@ public class LongSort {
         return storeIndex;
     }
     
-    public static int[] getIndexArray(long[] values) {
-        return IntSort.getIndexArray(values.length);
+    public static long[] getIndexArray(long[] values) {
+        return getIndexArray(values.length);
     }
 
+    /**
+     * Gets an array where array[i] = i.
+     * @param length The length of the array.
+     * @return The new index array.
+     */
+    public static long[] getIndexArray(int length) {
+        long[] index = new long[length];
+        for (int i=0; i<index.length; i++) {
+            index[i] = (long) i;
+        }
+        return index;
+    }
+    
     /**
      * Swaps the elements at positions i and j.
      */
