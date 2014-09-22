725f946f6e987ed0fa8a464dc3cc9b210f3bab93
Matt Gormley <mrg@cs.jhu.edu>
2014-09-22 12:25:33 -0400

    Bug fix: SafeCast wasn't checking for underflow.

--- src/main/java/edu/jhu/prim/util/SafeCast.java
+++ src/main/java/edu/jhu/prim/util/SafeCast.java
@@ -5,30 +5,37 @@ public class SafeCast {
     private SafeCast() {
         // Private constructor.
     }
+
+    public static byte safeIntToByte(int i) {
+        if (i > (int)Byte.MAX_VALUE || i < (int) Byte.MIN_VALUE) {
+            throw new IllegalStateException("Cannot convert int to byte: " + i);
+        }
+        return (byte)i;
+    }
     
     public static short safeIntToShort(int i) {
-        if (i > (int)Short.MAX_VALUE) {
+        if (i > (int)Short.MAX_VALUE || i < (int) Short.MIN_VALUE) {
             throw new IllegalStateException("Cannot convert int to short: " + i);
         }
         return (short)i;
     }
     
     public static int safeLongToInt(long l) {
-        if (l > (long)Integer.MAX_VALUE) {
+        if (l > (long)Integer.MAX_VALUE || l < (long) Integer.MIN_VALUE) {
             throw new IllegalStateException("Cannot convert long to int: " + l);
         }
         return (int)l;
     }
     
     public static int safeDoubleToInt(double d) {
-        if (d > (double)Integer.MAX_VALUE) {
+        if (d > (double)Integer.MAX_VALUE || d < (double) Integer.MIN_VALUE) {
             throw new IllegalStateException("Cannot convert double to int: " + d);
         }
         return (int)d;
     }
 
     public static float safeDoubleToFloat(double d) {
-        if (d > (double)Float.MAX_VALUE) {
+        if (Math.abs(d) > (double)Float.MAX_VALUE || Math.abs(d) < (double)Float.MIN_VALUE) {
             throw new IllegalStateException("Cannot convert double to float: " + d);
         }
         return (float)d;
